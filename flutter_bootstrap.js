bfe8cc1ebc01f6d3abe397e15e861a24440f5943
edukshare-max <tu-email@ejemplo.com>
2026-06-09 21:54:55 -0600

    deploy: publish UAGro redesign - 2026-06-09 21:54

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3231852195"
+    serviceWorkerVersion: "269894267"
   }
 });
